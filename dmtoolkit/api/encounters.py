1b153496f14aa2e2021f797993acc93c16157678
Ben Airey <38414634+ben-githubs@users.noreply.github.com>
2025-07-19 18:05:40 -0500

    Feature: Local caching for players and encounters (#7)
    * Add caching for players
    
    * Clean up player.py file
    
    * Fixing api and player adder in tracker
    
    * Make encounters cached as well

--- dmtoolkit/api/encounters.py
+++ dmtoolkit/api/encounters.py
@@ -1,57 +1,38 @@
 """ Code for storing, creating, and editing combat encounters."""
 
 import json
-from pathlib import Path
 from uuid import uuid4
 
-datadir = Path(__file__).parent / "data"
-
-_ENCOUNTERS = {}
-def _load_from_file(fname: Path = datadir / "encounters.json"):
-    """Load the encounter set from a file."""
-    global _ENCOUNTERS
-    with fname.open("r") as f:
-        _ENCOUNTERS = json.load(f)
-
-def _save_to_file(fname: Path = datadir / "encounters.json"):
-    """Save the current set of encounters to a file."""
-    global _ENCOUNTERS
-    with fname.open("w") as f:
-        json.dump(_ENCOUNTERS, f)
-
-_load_from_file()
-
-"""
- -- FUNCTION DEFINITIONS --
-"""
+from flask import request, Response
 
 def getall():
     """Return all encounters."""
-    global _ENCOUNTERS
-    return _ENCOUNTERS.copy()
+    return json.loads(request.cookies.get("encounters", "{}"))
 
 
 def get(eid: str):
     """Fetch a single encounter, using it's encounter ID."""
-    global _ENCOUNTERS
-    encounter: dict = _ENCOUNTERS.get(eid)
+    encounter: dict = getall().get(eid)
     if not encounter:
         return None
     return encounter.copy()
 
 
-def create_or_update(encounter: dict) -> str:
+def create_or_update(resp: Response, encounter: dict) -> str:
     """Save an encounter. Returns the UUID assigned to it."""
-    global _ENCOUNTERS
-    eid = encounter.get("id") or str(uuid4())
+    encounters = getall()
+    eid = encounters.get("id") or str(uuid4())
     encounter["id"] = eid
-    _ENCOUNTERS[eid] = encounter
-    _save_to_file()
+    encounters[eid] = encounter
+    _save_encounters(resp, encounters)
     return eid
 
 
-def delete(eid: str):
+def delete(resp: Response, eid: str):
     """Delete an encounter."""
-    global _ENCOUNTERS
-    _ENCOUNTERS.pop(eid)
-    _save_to_file()
+    encounters = getall()
+    encounters.pop(eid)
+    _save_encounters(resp, encounters)
+
+def _save_encounters(resp: Response, encounters: dict):
+    resp.set_cookie("encounters", json.dumps(encounters))
--- dmtoolkit/players/routes.py
+++ dmtoolkit/players/routes.py
@@ -1,9 +1,11 @@
-import json
+from dataclasses import asdict
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, make_response
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import InputRequired, NumberRange, ValidationError
 
-from dmtoolkit.api.players import list_players, create_player, get_player, update_player
-from dmtoolkit.players.forms import CreateForm, UpdateForm
+import dmtoolkit.api.players as api
 
 players_bp = Blueprint(
     "players_bp",
@@ -14,18 +16,44 @@ players_bp = Blueprint(
     url_prefix = "/players"
 )
 
+class CreateForm(FlaskForm):
+    name = StringField("Name", [InputRequired()])
+    ac = IntegerField("AC", [InputRequired(), NumberRange(min=0)])
+    hp = IntegerField("Max HP", [InputRequired(), NumberRange(min=1, message="No!")])
+    pp = IntegerField("Passive Perception", [InputRequired(), NumberRange(min=0)])
+    submit = SubmitField("Create Player Character")
+
+    def validate_name(self, field):
+        print("Validating...")
+        players = api.list_players()
+        if any(field.data == p.name for p in players):
+            print("Name already used")
+            raise ValidationError(f"Cannot create new player with name '{field.data}' because that name is already in use.")
+
+
+
+def UpdateForm():
+    form = CreateForm()
+    form.name.validators = ()
+    form.ac.validators = ()
+    form.hp.validators = ()
+    form.pp.validators = ()
+    form.submit.label.text = "Update Player"
+    return form
+
 @players_bp.route("/list")
 def list_players_page():
     page = {
         "title": "DMTTools - Players"
     }
-    players = list_players()
+    players = api.list_players()
     return render_template("list_players.jinja2", page=page, players=players)
 
 
 @players_bp.route("/new", methods=["GET", "POST"])
 def new_player_page():
     form = CreateForm()
+    print(form.validate_on_submit())
     if form.validate_on_submit():
         player = {
             "name": form.name.data,
@@ -33,8 +61,10 @@ def new_player_page():
             "pp": form.pp.data,
             "hp": form.hp.data
         }
-        create_player(**player)
-        return redirect(url_for("players_bp.list_players_page"))
+        print("NEW")
+        resp = make_response(redirect(url_for("players_bp.list_players_page")))
+        api.create_player(resp, player)
+        return resp
     
     page = {
         "title": "DMTools - New Player"
@@ -45,9 +75,11 @@ def new_player_page():
 @players_bp.route("/edit/<player_name>", methods=["GET","POST"])
 def update_player_page(player_name: str):
     form = UpdateForm()
-    player = get_player(player_name)
-    if not player:
+    players = api.list_players()
+    player_arr = [p for p in players if p.name == player_name]
+    if not player_arr:
         return "404 Player Not Found"
+    player = player_arr[0]
     if form.validate_on_submit():
         player_params = {}
         if val := form.name.data:
@@ -58,10 +90,27 @@ def update_player_page(player_name: str):
             player_params["hp"] = val
         if val := form.pp.data:
             player_params["pp"] = val
-        update_player(player_name, **player_params)
-        return redirect(url_for("players_bp.list_players_page"))
+
+        player_dict = asdict(player)
+        player_dict |= player_params # Update player params
+        new_player = api.Player(**player_dict)
+        if new_player.name != player.name and any(p.name == new_player.name for p in players):
+            # TODO: Handle this more gracefully
+            raise ValueError(f"Cannot rename player to '{new_player.name}' as this name is already in use")
+        idx = players.index(player)
+        players[idx] = new_player
+
+        resp = make_response(redirect(url_for("players_bp.list_players_page")))
+        api._save_players(resp, players)
+        return resp
     
     page = {
         "title": f"DMTools - Edit {player_name}"
     }
     return render_template("edit_player.jinja2", page=page, player=player, form=form)
+
+@players_bp.route("/delete/<player_name>", methods=["GET"])
+def delete_player(player_name: str):
+    resp = make_response(redirect(url_for("players_bp.list_players_page")))
+    api.delete_player(resp, player_name)
+    return resp
