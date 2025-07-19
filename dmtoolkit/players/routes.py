927d813b08f72f6d738484d5a3b3fdb72e48cc31
Ben Airey <benjaminjohnairey@gmail.com>
2025-07-19 16:53:13 -0500

    Add caching for players

--- dmtoolkit/players/routes.py
+++ dmtoolkit/players/routes.py
@@ -1,9 +1,12 @@
+from dataclasses import asdict
 import json
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, Response, make_response
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
 
-from dmtoolkit.api.players import list_players, create_player, get_player, update_player
-from dmtoolkit.players.forms import CreateForm, UpdateForm
+from dmtoolkit.api.players import Player
 
 players_bp = Blueprint(
     "players_bp",
@@ -14,18 +17,47 @@ players_bp = Blueprint(
     url_prefix = "/players"
 )
 
+class CreateForm(FlaskForm):
+    name = StringField("Name", [DataRequired()])
+    ac = IntegerField("AC", [DataRequired()])
+    hp = IntegerField("Max HP", [DataRequired()])
+    pp = IntegerField("Passive Perception", [DataRequired()])
+    submit = SubmitField("Create Player Character")
+
+    def validate_name(self, field):
+        players = _load_players()
+        if any(field.data == p.name for p in players):
+            raise ValidationError(f"Cannot create new player with name '{field.data}' because that name is already in use.")
+
+
+
+def UpdateForm():
+    form = CreateForm()
+    form.name.validators = ()
+    form.ac.validators = ()
+    form.hp.validators = ()
+    form.pp.validators = ()
+    form.submit.label.text = "Update Player"
+    return form
+
 @players_bp.route("/list")
 def list_players_page():
     page = {
         "title": "DMTTools - Players"
     }
-    players = list_players()
+    players = _load_players()
     return render_template("list_players.jinja2", page=page, players=players)
 
 
 @players_bp.route("/new", methods=["GET", "POST"])
 def new_player_page():
+    players = _load_players()
     form = CreateForm()
+    def validate_name(_, field):
+        print("I AM CALLED")
+        if any(field.data == p.name for p in players):
+            raise ValueError(f"Cannot create new player with name '{new_player.name}' because that name is already in use.")
+    setattr(form, "validate_name", validate_name)
     if form.validate_on_submit():
         player = {
             "name": form.name.data,
@@ -33,8 +65,11 @@ def new_player_page():
             "pp": form.pp.data,
             "hp": form.hp.data
         }
-        create_player(**player)
-        return redirect(url_for("players_bp.list_players_page"))
+        new_player = (Player(**player))
+        players.append(new_player)
+        resp = make_response(redirect(url_for("players_bp.list_players_page")))
+        _save_players(resp, players)
+        return resp
     
     page = {
         "title": "DMTools - New Player"
@@ -45,9 +80,11 @@ def new_player_page():
 @players_bp.route("/edit/<player_name>", methods=["GET","POST"])
 def update_player_page(player_name: str):
     form = UpdateForm()
-    player = get_player(player_name)
-    if not player:
+    players = _load_players()
+    player_arr = [p for p in players if p.name == player_name]
+    if not player_arr:
         return "404 Player Not Found"
+    player = player_arr[0]
     if form.validate_on_submit():
         player_params = {}
         if val := form.name.data:
@@ -58,10 +95,36 @@ def update_player_page(player_name: str):
             player_params["hp"] = val
         if val := form.pp.data:
             player_params["pp"] = val
-        update_player(player_name, **player_params)
-        return redirect(url_for("players_bp.list_players_page"))
+
+        player_dict = asdict(player)
+        player_dict |= player_params # Update player params
+        new_player = Player(**player_dict)
+        if new_player.name != player.name and any(p.name == new_player.name for p in players):
+            # TODO: Handle this more gracefully
+            raise ValueError(f"Cannot rename player to '{new_player.name}' as this name is already in use")
+        idx = players.index(player)
+        players[idx] = new_player
+
+        resp = make_response(redirect(url_for("players_bp.list_players_page")))
+        _save_players(resp, players)
+        return resp
     
     page = {
         "title": f"DMTools - Edit {player_name}"
     }
     return render_template("edit_player.jinja2", page=page, player=player, form=form)
+
+@players_bp.route("/delete/<player_name>", methods=["GET"])
+def delete_player(player_name: str):
+    players = _load_players()
+    new_players = [p for p in players if p.name != player_name]
+    resp = make_response(redirect(url_for("players_bp.list_players_page")))
+    _save_players(resp, new_players)
+    return resp
+
+def _load_players() -> list[Player]:
+    player_specs = json.loads(request.cookies.get("players", "[]"))
+    return [Player(**spec) for spec in player_specs]
+
+def _save_players(response: Response, players: list[Player]) -> None:
+    response.set_cookie("players", json.dumps([asdict(player) for player in players]))
